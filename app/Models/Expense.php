28500be68cdaea575c17ffdacbb1aeea7c496afd
Erik Boinski <161290672+erik-io@users.noreply.github.com>
2025-10-13 17:55:09 +0200

    Add user relationship methods to Expense and User models

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Expense extends Model
 {
-    //
+    /**
+     * Get the user that owns the expense.
+     *
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class); // $expense->user
+    }
 }
