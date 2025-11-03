15c5b423d488d1ee4c4f16c6139de791f5255946
Erik Boinski <161290672+erik-io@users.noreply.github.com>
2025-11-03 13:48:33 +0100

    Add RejectExpenseRequest for validating rejection_comment field

---
+++ app/Http/Requests/RejectExpenseRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class RejectExpenseRequest extends FormRequest
+{
+    /**
+     * Authorization is already handled by the middleware.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'rejection_comment' => 'required|string|max:5000'
+        ];
+    }
+}
