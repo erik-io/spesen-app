a96f146423387917cd8d373843219b82530703d4
Erik Boinski <161290672+erik-io@users.noreply.github.com>
2025-11-07 10:01:20 +0100

    Add CreatesApplication trait for testing setup.

---
+++ tests/CreatesApplication.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Trait CreatesApplication
+ *
+ * This trait provides a method to create the application instance
+ * used by the testing environment in a Laravel application.
+ *
+ * Responsibilities:
+ * - Loads the application bootstrap script.
+ * - Boots up the kernel for the application.
+ *
+ * @method Application createApplication()
+ */
+
+namespace Tests;
+
+use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Foundation\Application;
+
+
+trait CreatesApplication
+{
+    /**
+     * Creates the application.
+     */
+    public function createApplication(): Application
+    {
+        // Load the application bootstrap script.
+        $app = require __DIR__ . '/../bootstrap/app.php';
+
+        // Boot up the kernel for the application.
+        $app->make(Kernel::class)->bootstrap();
+
+        return $app;
+    }
+}
