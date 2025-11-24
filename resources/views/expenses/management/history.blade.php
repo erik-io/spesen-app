5c034478773b8e7cf14e21bb6846e0985cfd0f6f
Erik Boinski <161290672+erik-io@users.noreply.github.com>
2025-11-24 11:03:49 +0100

    Remove 'Archive' entry from localization files and update header to 'Expense History'

--- resources/views/expenses/management/history.blade.php
+++ resources/views/expenses/management/history.blade.php
@@ -5,7 +5,7 @@
     </x-slot>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Archive') }}
+            {{ __('Expense History') }}
         </h2>
     </x-slot>
 
